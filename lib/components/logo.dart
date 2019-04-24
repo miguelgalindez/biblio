43b9aadcfea4867f407fe799f057e466166183e3
Miguel Angel Galindez <miguelgalindez@unicauca.edu.co>
2019-04-24 12:05:49 -0500

    [login-page] Added dark background and created animated app logo

--- lib/components/logo.dart
+++ lib/components/logo.dart
@@ -1,17 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:animator/animator.dart';
 
 class Logo extends StatelessWidget {
   final AssetImage image;
   final double height;
   final double width;
+  final Duration animationDuration;
 
-  Logo({Key key, @required this.image, this.height = 200.0, this.width = 200.0})
+  Logo(
+      {Key key,
+      @required this.image,
+      this.height = 200.0,
+      this.width = 200.0,
+      this.animationDuration})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return FittedBox(
-      child: Image(height: height, width: width, image: image),
+    return Animator(
+      duration: animationDuration,
+      curve: Curves.bounceInOut,
+      tween: Tween(begin: 0.01, end: 1.0),
+      builder: (Animation animation) => Transform.scale(
+            scale: animation.value,
+            child: Image(
+              height: height,
+              width: width,
+              image: image,
+            ),
+          ),      
     );
   }
 }
--- lib/screens/login/index.dart
+++ lib/screens/login/index.dart
@@ -6,9 +6,12 @@ import 'package:biblio/screens/login/animatedButton.dart';
 import 'package:biblio/models/User.dart';
 import 'package:biblio/screens/home/index.dart';
 
-
 class LoginScreen extends StatefulWidget {
-  final Widget logo = Logo(image: AssetImage("assets/appLogo.png"));
+  final Logo logo = Logo(
+    image: const AssetImage("assets/appLogo.png"),
+    animationDuration: const Duration(seconds: 1),
+  );  
+
   final double horizontalPadding = 24.0;
   // Create a global key that will uniquely identify the Form widget and allow
   // us to validate the form
@@ -46,35 +49,71 @@ class _LoginScreenState extends State<LoginScreen>
   }
 
   Function _handleAnimationCompleted(BuildContext context) => () {
-    Navigator.pushReplacement(
+        Navigator.pushReplacement(
             context, MaterialPageRoute(builder: (context) => Home()));
-    };
+      };
 
   @override
   Widget build(BuildContext context) {
     MediaQueryData mediaQueryData = MediaQuery.of(context);
-
+    Color backgroundColor = Colors.black.withOpacity(0.77);
     return SafeArea(
       child: Scaffold(
-        resizeToAvoidBottomPadding: true,
-        backgroundColor: Color(0xff01305c),
-        body: Center(
-          child: SingleChildScrollView(
-            child: Stack(
-              alignment: AlignmentDirectional.bottomCenter,
+        //resizeToAvoidBottomPadding: true,
+        backgroundColor: backgroundColor,
+        body: Stack(
+          fit: StackFit.expand,
+          alignment: AlignmentDirectional.bottomCenter,
+          children: <Widget>[
+            Image(
+              image: AssetImage("assets/login-background.jpg"),
+              fit: BoxFit.cover,
+              color: backgroundColor,
+              colorBlendMode: BlendMode.darken,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: widget.horizontalPadding,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  widget.logo,
+                  SizedBox(height: 40.0),
+                  LoginForm(
+                    onFormChange: handleFormChange,
+                    formKey: widget._formKey,
+                  ),
+                  SizedBox(height: 80.0),
+                  AnimatedButton(
+                    text: "Iniciar sesión",
+                    username: username,
+                    password: password,
+                    formKey: widget._formKey,
+                    onAnimationCompleted: _handleAnimationCompleted(context),
+                    screenSize: mediaQueryData.size,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+/**
+ * Column(
               children: <Widget>[
-                Column(
-                  children: <Widget>[
-                    widget.logo,
-                    SizedBox(height: 20.0),
-                    LoginForm(
-                      horizontalPadding: widget.horizontalPadding,
-                      onFormChange: handleFormChange,
-                      formKey: widget._formKey,
-                    ),
-                    SizedBox(height: 80.0),
-                  ],
+                widget.logo,
+                SizedBox(height: 20.0),
+                LoginForm(
+                  horizontalPadding: widget.horizontalPadding,
+                  onFormChange: handleFormChange,
+                  formKey: widget._formKey,
                 ),
+                SizedBox(height: 80.0),
                 AnimatedButton(
                   text: "Iniciar sesión",
                   horizontalPadding: widget.horizontalPadding,
@@ -86,9 +125,4 @@ class _LoginScreenState extends State<LoginScreen>
                 ),
               ],
             ),
-          ),
-        ),
-      ),
-    );
-  }
-}
+ */
